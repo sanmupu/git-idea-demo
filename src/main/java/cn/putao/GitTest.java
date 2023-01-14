91fe3cc2fe4972680b525eb19dd6ccdec1e8f7ed
sanmupu <122533094+sanmupu@users.noreply.github.com>
2023-01-14 15:48:56 +0800

    Update GitTest.java

--- src/main/java/cn/putao/GitTest.java
+++ src/main/java/cn/putao/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello Git3");
         System.out.println("dev Test");
         System.out.println("test1");
+        System.out.println("pull");
     }
 }
